b5e502b6f16ff1c1a709fea7d99843b6e5bf59f2
Надим <aversul@gmail.com>
2023-02-22 12:27:13 +0400

    пагинация на уровне проекта

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import User
-from .serializers import CustomUserSerializer
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
+
+from .models import User
+from .serializers import CustomUserSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = PageNumberPagination
+    
