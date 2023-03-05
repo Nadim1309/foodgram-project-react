56240f40b93aae747ddb0916e4daf992666a5460
Надим Шакиржанов~ <aversul@gmail.com>
2023-03-05 14:13:20 +0400

    перед подключением GitHubAction

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# DATABASES = {
-#    'default': {
-#        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#        'NAME': os.getenv('DB_NAME', 'postgres'),
-#        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-#        'HOST': os.getenv('DB_HOST', 'localhost'),
-#        'PORT': os.getenv('DB_PORT', '5432')
-#    }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 # Password validation
