72a0573561e1572981046879d2cae2ac0dbb0139
Надим Шакиржанов~ <aversul@gmail.com>
2023-03-05 15:15:02 +0400

    подключение GitHubAction_2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PAGE_SAZE_NORMAL = 6
 PAGE_SAZE_LARGE = 20
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings -  unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
