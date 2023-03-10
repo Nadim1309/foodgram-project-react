7ebc425353c1abf9089ac31fcf49be18e8ca9048
Надим Шакиржанов~ <aversul@gmail.com>
2023-03-10 19:11:00 +0400

    v9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,7 +155,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
