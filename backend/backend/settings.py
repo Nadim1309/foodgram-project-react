ac86d2ffb1758625ac4288344493890f174f9aa3
Надим Шакиржанов~ <aversul@gmail.com>
2023-03-11 15:08:27 +0400

    v10

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,10 +146,10 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 # Default primary key field type
