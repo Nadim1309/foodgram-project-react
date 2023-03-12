33918b1d9eaaec22fd1ee6122f3a939bda931599
Надим Шакиржанов~ <aversul@gmail.com>
2023-03-12 13:26:05 +0400

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.1/ ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
